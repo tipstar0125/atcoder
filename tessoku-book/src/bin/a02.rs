79131d51cd1e630cfd0ba67fd59176a6301150ae
Yoke <tayok@synspective.com>
2022-09-19 11:59:06 +0900

    a02

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -1,3 +1,14 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        x: u32,
+        a: [u32; n]
+    }
+
+    if a.iter().any(|e| *e == x) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
