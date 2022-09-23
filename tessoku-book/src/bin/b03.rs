b4f147d33da93f7fd08127db0bd8cf7f6aa8e6b3
Yoke <tayok@synspective.com>
2022-09-23 11:55:34 +0900

    b03

--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: u32,
+        A: [u32; N]
+    }
+
+    let mut exists = false;
+    for i in 0..A.len() {
+        for j in i+1..A.len() {
+            for k in j+1..A.len() {
+                if A[i] + A[j] + A[k] == 1000 {
+                    exists = true;
+                }
+            }
+        }
+    }
+
+    if exists {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
