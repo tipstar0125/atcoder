1d07f408a2200f9bb476a37d22cc55d504f41d8b
Yoke <tayok@synspective.com>
2022-09-23 13:29:44 +0900

    b04

--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,3 +1,16 @@
+use proconio::{input, marker::Chars};
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: Chars
+    }
+    
+    let mut ans = 0;
+    for (i, &c) in N.iter().rev().enumerate() {
+        if c == '1' {
+           ans += 1 << i;
+        }
+    }
+    println!("{:?}", ans);
 }
