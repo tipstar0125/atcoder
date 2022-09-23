657c3c900a3e2b695642f166b5784375dd837f1d
Yoke <tayok@synspective.com>
2022-09-23 12:38:39 +0900

    a04

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -1,3 +1,19 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        mut N: u32
+    }
+
+    let mut vec_deque: VecDeque<String> = VecDeque::new();
+    for _ in 0..10 {
+        let m = N % 2;
+        vec_deque.push_front(m.to_string());
+        N /= 2;
+    }
+    let vec = Vec::from(vec_deque);
+    println!("{:?}", vec.join(""));
 }
