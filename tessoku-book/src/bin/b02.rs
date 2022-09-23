8dcf61829694342d8552b78d71487301091db785
Yoke <tayok@synspective.com>
2022-09-23 11:34:34 +0900

    b02

--- tessoku-book/src/bin/b02.rs
+++ tessoku-book/src/bin/b02.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32
+    }
+
+    let mut exists = false;
+    for i in a..=b {
+        if (100 % i) == 0 {
+            exists = true;
+            break;
+        }
+    }
+    if exists {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
