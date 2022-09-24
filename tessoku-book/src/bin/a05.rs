7603c1b86efe6c060723fcc2f5f6e06d19c13b32
Yoke <tayok@synspective.com>
2022-09-24 14:02:52 +0900

    a05

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: isize,
+        K: isize
+    }
+
+    let mut ans = 0;
+    for i in 1..=N {
+        for j in 1..=N {
+            let x = K - i - j;
+            if x >= 1 && x <= N {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{:?}", ans);
 }
