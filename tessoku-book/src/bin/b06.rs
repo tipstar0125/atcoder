7c5b582babf22bd96aee0b7e97ce4b68b530faf2
Yoke <tayok@synspective.com>
2022-09-24 17:31:23 +0900

    b06

--- tessoku-book/src/bin/b06.rs
+++ tessoku-book/src/bin/b06.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+use std::cmp::Ordering;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N],
+        Q: usize,
+        LR:[(usize, usize); Q]
+    }
+
+    let mut atari = vec![0];
+    let mut hazre = vec![0];
+
+    for i in 0..N {
+        if A[i] == 0 {
+            atari.push(atari[i]);
+            hazre.push(hazre[i] + 1);
+        } else {
+            atari.push(atari[i] + 1);
+            hazre.push(hazre[i]);
+        }
+    }
+
+    for &(L, R) in &LR {
+        let num_atari = atari[R] - atari[L - 1];
+        let num_hazre = hazre[R] - hazre[L - 1];
+
+        match num_atari.cmp(&num_hazre) {
+            Ordering::Greater => println!("win"),
+            Ordering::Less => println!("lose"),
+            Ordering::Equal => println!("draw"),
+        }
+    }
 }
