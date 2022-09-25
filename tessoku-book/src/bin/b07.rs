03fcc3dce966a9acf1e56e08ce88c35578f19153
Yoke <tayok@synspective.com>
2022-09-25 10:53:14 +0900

    b07

--- tessoku-book/src/bin/b07.rs
+++ tessoku-book/src/bin/b07.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
+#[allow(non_snake_case)]
 fn main() {
-    todo!();
+    input! {
+        T: usize,
+        N: usize,
+        LR: [(usize, usize); N]
+    }
+
+    let mut B = vec![0; T + 1];
+    for &(L, R) in &LR {
+        B[L] += 1;
+        B[R] -= 1;
+    }
+
+    let mut ans = 0;
+    for b in &B[..T] {
+        ans += b;
+        println!("{:?}", ans);
+    }
 }
