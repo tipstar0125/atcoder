0da1f553536a81157d19a5f44b59e7a78ece5d0c
Yoke <tayok@synspective.com>
2022-10-01 17:15:33 +0900

    b08

--- tessoku-book/src/bin/b08.rs
+++ tessoku-book/src/bin/b08.rs
@@ -1,3 +1,38 @@
+use proconio::{fastout, input};
+
+const MAX: usize = 1500;
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        XY: [(usize, usize); N],
+        Q: usize,
+        abcd: [(usize, usize, usize, usize); Q]
+    }
+
+    let mut S = vec![vec![0; MAX + 1]; MAX + 1];
+
+    for &(X, Y) in &XY {
+        S[X][Y] += 1;
+    }
+
+    let mut T = vec![vec![0; MAX + 1]; MAX + 1];
+
+    for i in 1..=MAX {
+        for j in 1..=MAX {
+            T[i][j] = T[i][j - 1] + S[i][j];
+        }
+    }
+    for j in 1..=MAX {
+        for i in 1..=MAX {
+            T[i][j] += T[i - 1][j];
+        }
+    }
+
+    for &(a, b, c, d) in &abcd {
+        let ans = T[c][d] + T[a - 1][b - 1] - T[a - 1][d] - T[c][b - 1];
+        println!("{:?}", ans);
+    }
 }
