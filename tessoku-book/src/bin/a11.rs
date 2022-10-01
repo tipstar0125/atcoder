33a51913f356a2a33256aecd2be186c5bb6b9ddc
Yoke <tayok@synspective.com>
2022-10-01 12:01:16 +0900

    a11 mofified

--- tessoku-book/src/bin/a11.rs
+++ tessoku-book/src/bin/a11.rs
@@ -10,23 +10,23 @@ fn main() {
         A: [usize; N]
     }
 
-    let ans = binary_search(X, A).unwrap() + 1;
+    let ans = binary_search(&X, &A).unwrap() + 1;
     println!("{:?}", ans);
 }
 
-fn binary_search(x: usize, v: Vec<usize>) -> Option<usize> {
+fn binary_search(x: &usize, v: &Vec<usize>) -> Option<usize> {
     let mut left = 0;
     let mut right = v.len() - 1;
 
     while left <= right {
         let med = (left + right) / 2;
-        if x < v[med] {
+        if *x < v[med] {
             right = med - 1;
         }
-        if x == v[med] {
+        if *x == v[med] {
             return Some(med);
         }
-        if x >= v[med] {
+        if *x >= v[med] {
             left = med + 1;
         }
     }
--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -1,3 +1,37 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N],
+        Q: usize,
+        X: [usize; Q]
+    }
+
+    for x in &X {
+        let ans = lower_bound(x, A).unwrap();
+        println!("{:?}", ans);
+    }
+}
+
+fn lower_bound(&x: usize, v: Vec<usize>) -> Option<usize> {
+    let mut left = 0;
+    let mut right = v.len() - 1;
+
+    while left <= right {
+        let med = (left + right) / 2;
+        if x < v[med] {
+            right = med - 1;
+        }
+        if x == v[med] {
+            return Some(med);
+        }
+        if x >= v[med] {
+            left = med + 1;
+        }
+    }
+    None
 }
