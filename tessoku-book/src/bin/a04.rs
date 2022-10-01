691ea7937d50322423b7e39ef4229a4e96fc21a8
Yoke <tayok@synspective.com>
2022-10-01 18:26:07 +0900

    a09

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -12,13 +12,4 @@ fn main() {
         print!("{:?}", (N / wari) % 2);
     }
     println!();
-    // let mut vec: Vec<u32> = Vec::new();
-    // for _ in 0..10 {
-    //     let m = N % 2;
-    //     vec.push(m);
-    //     N /= 2;
-    // }
-    // for v in vec.iter().rev() {
-    //     print!("{:?}", v);
-    // }
 }
--- tessoku-book/src/bin/a09.rs
+++ tessoku-book/src/bin/a09.rs
@@ -1,3 +1,42 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        H: usize,
+        W: usize,
+        N: usize,
+        ABCD: [(usize, usize, usize, usize); N]
+    }
+
+    let mut X = vec![vec![0; W + 2]; H + 2];
+
+    for &(A, B, C, D) in &ABCD {
+        X[A][B] += 1;
+        X[A][D + 1] -= 1;
+        X[C + 1][B] -= 1;
+        X[C + 1][D + 1] += 1;
+    }
+
+    let mut Z = vec![vec![0; W + 1]; H + 1];
+    for i in 1..=H {
+        for j in 1..=W {
+            Z[i][j] = Z[i][j - 1] + X[i][j];
+        }
+    }
+    for j in 1..=W {
+        for i in 1..=H {
+            Z[i][j] += Z[i - 1][j];
+        }
+    }
+
+    for row in &Z[1..] {
+        let ans = row[1..]
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+        println!("{}", ans);
+    }
 }
