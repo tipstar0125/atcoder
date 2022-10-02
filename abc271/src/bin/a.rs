2a99a94bda6ac0e960acb757fe448cb16ba0b1c5
Yoke <tayok@synspective.com>
2022-10-02 17:12:52 +0900

    abc271 a

--- abc271/src/bin/a.rs
+++ abc271/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+    }
+    let num_str = "0123456789ABCDEF";
+    let ten = N / 16 % 16;
+    let one = N % 16;
+    println!("{}{}", num_str.chars().nth(ten).unwrap(), num_str.chars().nth(one).unwrap());
 }
