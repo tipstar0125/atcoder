9465475201318796d8d95648e945707687d22b74
Yoke <tayok@synspective.com>
2022-10-02 11:50:06 +0900

    a12

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,3 +1,30 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        K: usize,
+        A: [usize; N]
+    }
+
+    let mut left = 1;
+    let mut right = 1_000_000_000;
+
+    while left < right {
+        let med = (left + right) / 2;
+        let mut sum = 0;
+        for a in &A {
+            sum += med / a;
+        }
+        if sum >= K {
+            right = med;
+        } else {
+            left = med + 1;
+        }
+    }
+
+    println!("{}", left);
 }
