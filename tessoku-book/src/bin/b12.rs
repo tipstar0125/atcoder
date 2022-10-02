1a2f3a623acb17db2c4e7c0cc41c269e3ddf27c6
Yoke <tayok@synspective.com>
2022-10-02 12:52:22 +0900

    b12

--- tessoku-book/src/bin/b12.rs
+++ tessoku-book/src/bin/b12.rs
@@ -1,3 +1,24 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: f64,
+    }
+
+    let mut left = 0_f64;
+    let mut right = 100_f64;
+
+    for _ in 0..20 {
+        let med = (left + right) / 2.0;
+        let value = med.powf(3.0) + med;
+        if value < N {
+            left = med;
+        } else {
+            right = med;
+        }
+    }
+
+    println!("{}", left);
 }
