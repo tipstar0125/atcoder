8d6da169e613b4cd742fac864a4a2afad72a15b6
Yoke <tayok@synspective.com>
2022-10-05 18:15:22 +0900

    b

--- abc271/src/bin/b.rs
+++ abc271/src/bin/b.rs
@@ -1,3 +1,27 @@
+use im_rc::HashMap;
+use proconio::{fastout, input, marker::Usize1};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        Q: usize
+    }
+    
+    let mut a = vec![];
+    for _ in 0..N {
+        input! {
+            l_i: usize,
+            a_i: [usize; l_i]
+        }
+        a.push(a_i)
+    }
+    input! {
+        st: [(Usize1, Usize1); Q]
+    }
+    
+    for &(s, t) in &st {
+        println!("{:?}", a[s][t]);
+    }
 }
