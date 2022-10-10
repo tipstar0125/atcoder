e49e53c6b3336ad23b77b1c41f1a3af6aec61093
Yoke <tayok@synspective.com>
2022-10-10 14:03:49 +0900

    a16

--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -1,3 +1,20 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N],
+        D: usize,
+        LR: [(usize, usize); D]
+    }
+
+    for (L, R) in LR {
+        let mut B = A.clone();
+        B.drain(L - 1..R);
+        B.sort();
+        println!("{:?}", B.last().unwrap());
+    }
 }
--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,3 +1,23 @@
+use proconio::{fastout, input};
+use std::cmp;
+
+#[allow(non_snake_case)]
+#[fastout]
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N-1],
+        B: [usize; N-2]
+    }
+
+    let mut dp = vec![0; N + 1];
+    dp[1] = 0;
+    dp[2] = A[0];
+
+    for i in 3..=N {
+        dp[i] = cmp::min(dp[i - 1] + A[i - 2], dp[i - 2] + B[i - 3]);
+    }
+
+    println!("{:?}", dp[N]);
 }
