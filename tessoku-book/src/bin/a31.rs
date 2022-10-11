f1e7f41e8675b99bfd8702ed5090d4b92cca7414
Yoke <tayok@synspective.com>
2022-10-11 18:25:22 +0900

    a31, b31

--- tessoku-book/src/bin/a31.rs
+++ tessoku-book/src/bin/a31.rs
@@ -1,3 +1,14 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N:usize
+    }
+
+    let num_3 = N / 3;
+    let num_5 = N / 5;
+    let num_15 = N / 15;
+    println!("{}", num_3 + num_5 - num_15);
 }
--- tessoku-book/src/bin/b31.rs
+++ tessoku-book/src/bin/b31.rs
@@ -1,3 +1,22 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N:usize
+    }
+
+    let num_3 = N / 3;
+    let num_5 = N / 5;
+    let num_7 = N / 7;
+    let num_15 = N / 15;
+    let num_21 = N / 21;
+    let num_35 = N / 35;
+    let num_105 = N / 105;
+
+    println!(
+        "{}",
+        num_3 + num_5 + num_7 - num_15 - num_21 - num_35 + num_105
+    );
 }
