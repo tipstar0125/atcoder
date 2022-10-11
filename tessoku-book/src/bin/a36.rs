d5f07173133fe51b4514e6267156d464894b5be2
Yoke <tayok@synspective.com>
2022-10-11 17:28:50 +0900

    a36

--- tessoku-book/src/bin/a36.rs
+++ tessoku-book/src/bin/a36.rs
@@ -1,3 +1,16 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        K: usize
+    }
+
+    if N * 2 - 2 <= K && K % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
