0d3e876272d1da9b8ba8d67577b26f10b18cb0a7
Yoke <tayok@synspective.com>
2022-10-11 17:59:30 +0900

    b36

--- tessoku-book/src/bin/b36.rs
+++ tessoku-book/src/bin/b36.rs
@@ -1,3 +1,24 @@
+use proconio::{fastout, input, marker::Chars};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        _: usize,
+        K: usize,
+        S: Chars
+    }
+
+    let mut num_on = 0;
+    for s in S {
+        if s == '1' {
+            num_on += 1;
+        }
+    }
+
+    if K % 2 == num_on % 2 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
