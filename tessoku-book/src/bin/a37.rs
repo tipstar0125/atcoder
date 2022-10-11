86183b8f026f952b3a95fe3e3b9d580809f832c7
Yoke <tayok@synspective.com>
2022-10-11 19:28:46 +0900

    a37

--- tessoku-book/src/bin/a37.rs
+++ tessoku-book/src/bin/a37.rs
@@ -1,3 +1,18 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        M: usize,
+        B: usize,
+        A: [usize; N],
+        C: [usize; M]
+    }
+
+    let mut ans = A.iter().sum::<usize>() * M;
+    ans += C.iter().sum::<usize>() * N;
+    ans += B * N * M;
+    println!("{:?}", ans);
 }
