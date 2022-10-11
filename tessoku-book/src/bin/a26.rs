faf405a8a43f4a98d07889f3e4de255e45aecd77
Yoke <tayok@synspective.com>
2022-10-11 09:48:26 +0900

    a26

--- tessoku-book/src/bin/a26.rs
+++ tessoku-book/src/bin/a26.rs
@@ -1,3 +1,23 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
+
 fn main() {
-    todo!();
+    input! {
+        Q: usize,
+        X: [usize; Q]
+    }
+
+    for &x in &X {
+        let mut ans = "Yes";
+        let mut i = 2;
+        while i * i <= x {
+            if x % i == 0 {
+                ans = "No";
+            }
+            i += 1;
+        }
+        println!("{}", ans);
+    }
 }
