6f908494f1c32b6c95462f3bb48193d645a7c68a
Yoke <tayok@synspective.com>
2022-10-11 10:40:19 +0900

    b26

--- tessoku-book/src/bin/b26.rs
+++ tessoku-book/src/bin/b26.rs
@@ -1,3 +1,29 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize
+    }
+
+    let mut is_prime_list = vec![true; N + 1];
+
+    let mut i = 2;
+    while i * i <= N {
+        if is_prime_list[i] {
+            let mut j = i * 2;
+            while j <= N {
+                is_prime_list[j] = false;
+                j += i;
+            }
+        }
+        i += 1;
+    }
+
+    for i in 2..=N {
+        if is_prime_list[i] {
+            println!("{:?}", i);
+        }
+    }
 }
