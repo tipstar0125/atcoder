a8ffc72c6e5c2a38c0a1f79b4b17f5a81445d2b2
Yoke <tayok@synspective.com>
2022-10-11 13:45:55 +0900

    a28

--- tessoku-book/src/bin/a28.rs
+++ tessoku-book/src/bin/a28.rs
@@ -1,3 +1,27 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        TA: [(String, isize); N]
+    }
+
+    let mut num = 0_isize;
+
+    for (T, A) in &TA {
+        match T.as_str() {
+            "+" => num += A,
+            "-" => num -= A,
+            "*" => num *= A,
+            _ => unreachable!(),
+        }
+
+        num %= 10000;
+        if num < 0 {
+            num += 10000;
+        }
+        println!("{:?}", num);
+    }
 }
