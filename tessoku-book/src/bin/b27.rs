b26288fb7c704d7f6f406d258aa849467b7a086a
Yoke <tayok@synspective.com>
2022-10-11 11:25:25 +0900

    b27

--- tessoku-book/src/bin/b27.rs
+++ tessoku-book/src/bin/b27.rs
@@ -1,3 +1,15 @@
+use num::integer::gcd;
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        mut A: usize,
+        mut B: usize,
+    }
+
+    let gcd = gcd(A, B);
+    let ans = A * B / gcd;
+    println!("{:?}", ans);
 }
