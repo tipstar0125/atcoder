725e728558f1948c92e7587fd7fd2b75ed497789
Yoke <tayok@synspective.com>
2022-10-12 16:32:39 +0900

    a53

--- tessoku-book/src/bin/a53.rs
+++ tessoku-book/src/bin/a53.rs
@@ -1,3 +1,43 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        Q: usize,
+    }
+
+    let mut Query = vec![];
+    for _ in 0..Q {
+        input! {
+            n: usize,
+        }
+        if n == 1 {
+            input! {
+               price: usize
+            }
+            Query.push((n, price));
+        } else {
+            Query.push((n, 0));
+        }
+    }
+
+    let mut heap = BinaryHeap::new();
+    for q in &Query {
+        match q {
+            (1, v) => heap.push(Reverse(v)),
+            (2, _) => {
+                if let Some(Reverse(v)) = heap.peek() {
+                    println!("{:?}", v);
+                }
+            }
+            (3, _) => {
+                heap.pop();
+            }
+            (_, _) => unreachable!(),
+        }
+    }
 }
