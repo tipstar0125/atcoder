b463bc554a9b5c87fc6e45b5668994a921355022
Yoke <tayok@synspective.com>
2022-10-12 11:37:53 +0900

    b52

--- tessoku-book/src/bin/b52.rs
+++ tessoku-book/src/bin/b52.rs
@@ -1,3 +1,34 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+use proconio::{fastout, input, marker::Chars};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        _: usize,
+        X: usize,
+        mut A: Chars
+    }
+
+    let mut queue = VecDeque::new();
+    queue.push_back(X - 1);
+    A[X - 1] = '@';
+
+    while !queue.is_empty() {
+        let pos = *queue.front().unwrap();
+
+        if pos > 0 && A[pos - 1] == '.' {
+            A[pos - 1] = '@';
+            queue.push_back(pos - 1);
+        }
+        if pos < A.len() - 1 && A[pos + 1] == '.' {
+            A[pos + 1] = '@';
+            queue.push_back(pos + 1);
+        }
+
+        queue.pop_front();
+    }
+    println!("{}", A.iter().join(""));
 }
