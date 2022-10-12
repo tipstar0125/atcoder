bb643dc836afc2d21361426cbd629718aa0a0e4e
Yoke <tayok@synspective.com>
2022-10-12 11:01:44 +0900

    a52

--- tessoku-book/src/bin/a52.rs
+++ tessoku-book/src/bin/a52.rs
@@ -1,3 +1,38 @@
+use std::collections::VecDeque;
+
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        Q: usize,
+    }
+
+    let mut Query = vec![];
+    for _ in 0..Q {
+        input! {
+            n: usize,
+        }
+        if n == 1 {
+            input! {
+               name: String
+            }
+            Query.push((n, name));
+        } else {
+            Query.push((n, "".to_string()));
+        }
+    }
+
+    let mut queue = VecDeque::new();
+    for q in &Query {
+        match q {
+            (1, v) => queue.push_back(v),
+            (2, _) => println!("{}", queue.front().unwrap()),
+            (3, _) => {
+                queue.pop_front();
+            }
+            (_, _) => unreachable!(),
+        }
+    }
 }
