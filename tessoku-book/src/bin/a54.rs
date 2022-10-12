5429a465ff5a05b9cb83138ed5b8e0971cb6a994
Yoke <tayok@synspective.com>
2022-10-12 16:58:30 +0900

    a54

--- tessoku-book/src/bin/a54.rs
+++ tessoku-book/src/bin/a54.rs
@@ -1,3 +1,41 @@
+use std::collections::HashMap;
+
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        Q: usize,
+    }
+
+    let mut Query = vec![];
+    for _ in 0..Q {
+        input! {
+            n: usize,
+        }
+        if n == 1 {
+            input! {
+                name: String,
+                score: usize
+            }
+            Query.push((n, name, score));
+        } else {
+            input! {
+                name: String,
+            }
+            Query.push((n, name, 0));
+        }
+    }
+
+    let mut map = HashMap::new();
+    for q in &Query {
+        match q {
+            (1, name, score) => {
+                map.insert(name, score);
+            }
+            (2, name, _) => println!("{:?}", map.get(name).unwrap()),
+            (_, _, _) => unreachable!(),
+        }
+    }
 }
