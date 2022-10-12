e1fa2237ef03b7622a457f6904c04061ec4ba046
Yoke <tayok@synspective.com>
2022-10-12 10:03:51 +0900

    a51

--- tessoku-book/src/bin/a51.rs
+++ tessoku-book/src/bin/a51.rs
@@ -1,3 +1,36 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        Q: usize,
+    }
+
+    let mut Query = vec![];
+    for _ in 0..Q {
+        input! {
+            n: usize,
+        }
+        if n == 1 {
+            input! {
+               name: String
+            }
+            Query.push((n, name));
+        } else {
+            Query.push((n, "".to_string()));
+        }
+    }
+
+    let mut stack = vec![];
+    for q in &Query {
+        match q {
+            (1, v) => stack.push(v),
+            (2, _) => println!("{}", stack.last().unwrap()),
+            (3, _) => {
+                stack.pop();
+            }
+            (_, _) => unreachable!(),
+        }
+    }
 }
