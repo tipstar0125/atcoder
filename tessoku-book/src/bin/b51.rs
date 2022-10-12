bfe07e8d327f004ffc68a813fe4905ef016da609
Yoke <tayok@synspective.com>
2022-10-12 10:53:04 +0900

    b51

--- tessoku-book/src/bin/b51.rs
+++ tessoku-book/src/bin/b51.rs
@@ -1,3 +1,20 @@
+use proconio::{fastout, input, marker::Chars};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        S: Chars
+    }
+
+    let mut stack = vec![];
+    for (i, c) in S.iter().enumerate() {
+        match c {
+            '(' => stack.push(i + 1),
+            ')' => {
+                println!("{} {}", stack.pop().unwrap(), i + 1);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
