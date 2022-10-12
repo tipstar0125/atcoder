ffb69ab10c029afbce919ad2f9a3e8e9b522db0f
Yoke <tayok@synspective.com>
2022-10-12 18:11:06 +0900

    b54

--- tessoku-book/src/bin/b54.rs
+++ tessoku-book/src/bin/b54.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N]
+    }
+
+    let mut map = HashMap::new();
+
+    let mut ans = 0;
+    for a in &A {
+        *map.entry(a).or_insert(0) += 1_usize;
+        ans += map[a] - 1;
+    }
+
+    println!("{:?}", ans);
 }
