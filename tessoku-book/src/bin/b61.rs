d88beb9db9916f4dba5725497c4c707a895cad44
Yoke <tayok@synspective.com>
2022-10-13 11:02:56 +0900

    b61

--- tessoku-book/src/bin/b61.rs
+++ tessoku-book/src/bin/b61.rs
@@ -1,3 +1,27 @@
+use proconio::{fastout, input};
+
+#[allow(non_snake_case)]
+#[allow(clippy::needless_range_loop)]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        M: usize,
+        AB: [(usize, usize); M]
+    }
+
+    let mut G = vec![vec![]; N + 1];
+
+    for &(A, B) in &AB {
+        G[A].push(B);
+        G[B].push(A);
+    }
+
+    let mut ans = 1;
+    for i in 2..=N {
+        if G[i].len() > G[ans].len() {
+            ans = i;
+        }
+    }
+    println!("{:?}", ans);
 }
