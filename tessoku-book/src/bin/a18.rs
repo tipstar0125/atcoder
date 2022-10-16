6a5560c9140ed71a5bfb0e1280872e5f64bf04fa
Yoke <tayok@synspective.com>
2022-10-16 12:50:14 +0900

    a18

--- tessoku-book/src/bin/a18.rs
+++ tessoku-book/src/bin/a18.rs
@@ -1,3 +1,42 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        S: usize,
+        A: [usize; N]
+    }
+
+    let mut dp = vec![vec![false; S + 1]; N + 1];
+    dp[0][0] = true;
+
+    for i in 1..=N {
+        let a = A[i - 1];
+        for j in 0..=S {
+            dp[i][j] |= dp[i - 1][j];
+            if dp[i - 1][j] && a + j <= S {
+                dp[i][a + j] = true;
+            }
+        }
+    }
+    let mut exists = false;
+
+    for i in 1..=N {
+        if dp[i][S] {
+            exists = true;
+        }
+    }
+
+    if exists {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
