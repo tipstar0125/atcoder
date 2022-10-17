827fcd5020ab8906ebb7181657ab2a92ffdce2c2
Yoke <tayok@synspective.com>
2022-10-17 14:40:12 +0900

    b40

--- tessoku-book/src/bin/b40.rs
+++ tessoku-book/src/bin/b40.rs
@@ -1,3 +1,28 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [usize; N]
+    }
+
+    let mut count = [0; 100];
+    for &a in &A {
+        count[a % 100] += 1;
+    }
+
+    let mut ans: isize = count[0] * (count[0] - 1) / 2;
+    ans += count[50] * (count[50] - 1) / 2;
+
+    for i in 1..=49 {
+        ans += count[i] * count[100 - i]
+    }
+
+    println!("{}", ans);
 }
