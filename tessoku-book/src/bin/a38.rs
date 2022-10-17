8ad435f0c87b75dec8f925894858a17dda010b97
Yoke <tayok@synspective.com>
2022-10-17 09:32:28 +0900

    a38

--- tessoku-book/src/bin/a38.rs
+++ tessoku-book/src/bin/a38.rs
@@ -1,3 +1,26 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        D: usize,
+        N: usize,
+        LRH: [(usize, usize, usize); N]
+    }
+
+    let mut L = vec![24; D + 1];
+
+    for &(l, r, h) in &LRH {
+        for d in l..=r {
+            L[d] = L[d].min(h);
+        }
+    }
+
+    let ans: usize = L[1..].iter().sum();
+    println!("{}", ans);
 }
