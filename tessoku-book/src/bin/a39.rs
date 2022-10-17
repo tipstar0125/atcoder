5b88df1d5a0e5b0efeb7fe596cd3e6bd2ed6004a
Yoke <tayok@synspective.com>
2022-10-17 10:51:31 +0900

    a39

--- tessoku-book/src/bin/a39.rs
+++ tessoku-book/src/bin/a39.rs
@@ -1,3 +1,27 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        mut LR: [(usize, usize); N]
+    }
+
+    let mut current_time = 0;
+    let mut ans = 0;
+    LR.sort_by(|a, b| a.1.cmp(&b.1));
+
+    for &(l, r) in &LR {
+        if current_time <= l {
+            current_time = r;
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
