8307118afb9c2994b195cf626fe4862e5b7a6ee9
Yoke <tayok@synspective.com>
2022-10-18 19:06:48 +0900

    c05

--- tessoku-book/src/bin/c05.rs
+++ tessoku-book/src/bin/c05.rs
@@ -1,3 +1,21 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize
+    }
+
+    let mut ans = "".to_string();
+    let lucky = "47";
+    for i in (0..10).rev() {
+        let index = (N - 1) / (1 << i) % 2;
+        ans += &lucky[index..index + 1];
+    }
+    println!("{}", ans);
 }
