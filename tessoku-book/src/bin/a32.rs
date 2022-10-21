2e2c9e38ad8c3defee3ec30d2ac71371542b988e
Yoke <tayok@synspective.com>
2022-10-21 15:27:47 +0900

    a32

--- tessoku-book/src/bin/a32.rs
+++ tessoku-book/src/bin/a32.rs
@@ -1,3 +1,31 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: usize,
+        B: usize
+    }
+
+    let mut dp = vec![false; N + 1];
+    for i in 0..=N {
+        if i >= A && !dp[i - A] {
+            dp[i] = true;
+        }
+        if i >= B && !dp[i - B] {
+            dp[i] = true;
+        }
+    }
+
+    if dp[N] {
+        println!("First");
+    } else {
+        println!("Second");
+    }
 }
