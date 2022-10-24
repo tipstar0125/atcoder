120d2b2a6ee05f2ff4b9887b0e2b75e72024866e
Yoke <tayok@synspective.com>
2022-10-24 19:00:04 +0900

    b19

--- tessoku-book/src/bin/b19_bekkai2.rs
+++ tessoku-book/src/bin/b19_bekkai2.rs
@@ -14,23 +14,31 @@ fn main() {
     }
 
     let V_MAX = 1000 * N;
-    let INF = 1 << 60;
-    let mut dp = vec![vec![INF; V_MAX + 1]; N + 1];
-    dp[0][0] = 0;
+    let mut dp = vec![vec![0; V_MAX + 1]; N + 1];
     for i in 1..=N {
         let (w, v) = wv[i - 1];
         for j in 0..=V_MAX {
             if j < v {
                 dp[i][j] = dp[i - 1][j];
             } else {
-                dp[i][j] = dp[i - 1][j].min(dp[i - 1][j - v] + w);
+                let k = dp[i - 1][j - v] + w;
+                if (j - v == 0 || dp[i - 1][j - v] != 0) && k <= W {
+                    dp[i][j] = k;
+                    if dp[i - 1][j] != 0 {
+                        dp[i][j] = dp[i - 1][j].min(k);
+                    } else {
+                        dp[i][j] = k;
+                    }
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
     }
 
     let mut ans = 0;
     for (i, &x) in dp[N].iter().enumerate() {
-        if x <= W {
+        if x != 0 {
             ans = i;
         }
     }
