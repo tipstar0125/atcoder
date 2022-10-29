255807694e7dea1fa849fb79ef82b1cad2aa61d6
Yoke <tayok@synspective.com>
2022-10-29 22:40:53 +0900

    abc275

---
+++ abc275/src/bin/c.rs
@@ -0,0 +1,136 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::comparison_chain)]
+#![allow(dead_code)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[macro_export]
+macro_rules! max {
+        ($x: expr) => ($x);
+        ($x: expr, $( $y: expr ),+) => {
+            std::cmp::max($x, max!($( $y ),+))
+        }
+    }
+#[macro_export]
+macro_rules! min {
+        ($x: expr) => ($x);
+        ($x: expr, $( $y: expr ),+) => {
+            std::cmp::min($x, min!($( $y ),+))
+        }
+    }
+#[macro_export]
+macro_rules! abs {
+    ($x: expr) => {
+        if $x < 0_isize {
+            $x * (-1)
+        } else {
+            $x
+        }
+    };
+}
+#[macro_export]
+macro_rules! absf {
+    ($x: expr) => {
+        if $x < 0.0 {
+            $x * (-1.0)
+        } else {
+            $x
+        }
+    };
+}
+
+#[derive(Debug, Clone)]
+struct UnionFind {
+    parent: Vec<isize>,
+    size: usize,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: vec![-1; n + 1],
+            size: n,
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] < 0 {
+            return x;
+        }
+        let root = self.find(self.parent[x] as usize);
+        self.parent[x] = root as isize;
+        root
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x == root_y {
+            return;
+        }
+        let size_x = -self.parent[root_x];
+        let size_y = -self.parent[root_y];
+        if size_x >= size_y {
+            self.parent[root_x] -= size_y;
+            self.parent[root_y] = root_x as isize;
+        } else {
+            self.parent[root_y] -= size_x;
+            self.parent[root_x] = root_y as isize;
+        }
+        self.size -= 1;
+    }
+
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+
+    fn is_root(&mut self, x: usize) -> bool {
+        self.find(x) == x
+    }
+
+    fn get_union_size(&mut self, x: usize) -> usize {
+        let root = self.find(x);
+        -self.parent[root] as usize
+    }
+
+    fn get_size(&self) -> usize {
+        self.size
+    }
+}
+
+#[fastout]
+fn main() {
+    input! {
+        S: [Chars; 9]
+    }
+
+    let mut ans = 0;
+
+    for i in 0..9 {
+        for j in 0..9 {
+            if S[i][j] == '#' {
+                for m in j + 1..9 {
+                    for n in i..9 {
+                        let diff_row = n - i;
+                        let diff_col = m - j;
+                        if n + diff_col < 9
+                            && m >= diff_row
+                            && i + diff_col < 9
+                            && j >= diff_row
+                            && S[n][m] == '#'
+                            && S[n + diff_col][m - diff_row] == '#'
+                            && S[i + diff_col][j - diff_row] == '#'
+                        {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
+}
