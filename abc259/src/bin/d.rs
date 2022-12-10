99812a5f9baec515f2506fe55d8f700622418b57
Yoke <tayok@synspective.com>
2022-12-10 17:15:35 +0900

    abc259-d

--- abc259/src/bin/d.rs
+++ abc259/src/bin/d.rs
@@ -1,3 +1,128 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
+    }
+}
+#[derive(Debug, Clone)]
+struct UnionFind {
+    parent: Vec<isize>,
+    size: usize,
+}
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: vec![-1; n + 1],
+            size: n,
+        }
+    }
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] < 0 {
+            return x;
+        }
+        let root = self.find(self.parent[x] as usize);
+        self.parent[x] = root as isize;
+        root
+    }
+    fn unite(&mut self, x: usize, y: usize) -> Option<(usize, usize)> {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x == root_y {
+            return None;
+        }
+        let size_x = -self.parent[root_x];
+        let size_y = -self.parent[root_y];
+        self.size -= 1;
+        if size_x >= size_y {
+            self.parent[root_x] -= size_y;
+            self.parent[root_y] = root_x as isize;
+            Some((root_x, root_y))
+        } else {
+            self.parent[root_y] -= size_x;
+            self.parent[root_x] = root_y as isize;
+            Some((root_y, root_x))
+        }
+    }
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+    fn is_root(&mut self, x: usize) -> bool {
+        self.find(x) == x
+    }
+    fn get_union_size(&mut self, x: usize) -> usize {
+        let root = self.find(x);
+        -self.parent[root] as usize
+    }
+    fn get_size(&self) -> usize {
+        self.size
+    }
+}
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            N: usize,
+            sx: isize,
+            sy: isize,
+            tx: isize,
+            ty: isize,
+            xyr: [(isize, isize, isize); N]
+        }
+
+        let mut start = 0;
+        let mut stop = 0;
+        for (i, &(x, y, r)) in xyr.iter().enumerate() {
+            if (sx - x) * (sx - x) + (sy - y) * (sy - y) == r * r {
+                start = i;
+            }
+            if (tx - x) * (tx - x) + (ty - y) * (ty - y) == r * r {
+                stop = i;
+            }
+        }
+
+        let mut uf = UnionFind::new(N);
+        for i in 0..N {
+            for j in i + 1..N {
+                let (x1, y1, r1) = xyr[i];
+                let (x2, y2, r2) = xyr[j];
+                let d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+                if d <= (r1 + r2) * (r1 + r2) && d >= (r1 - r2) * (r1 - r2) {
+                    uf.unite(i, j);
+                }
+            }
+        }
+
+        println!("{}", if uf.is_same(start, stop) { "Yes" } else { "No" });
+    }
+}
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
 }
