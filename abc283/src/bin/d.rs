1a842626bbd294b35d8ce77d7f1be622a0aeda24
Yoke <tayok@synspective.com>
2022-12-25 00:01:38 +0900

    abc283

---
+++ abc283/src/bin/d.rs
@@ -0,0 +1,131 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use std::collections::HashSet;
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
+    }
+}
+#[derive(Debug, Clone)]
+struct UnionFind {
+    parent: Vec<isize>,
+    size: usize,
+}
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: vec![-1; n + 1],
+            size: n,
+        }
+    }
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] < 0 {
+            return x;
+        }
+        let root = self.find(self.parent[x] as usize);
+        self.parent[x] = root as isize;
+        root
+    }
+    fn unite(&mut self, x: usize, y: usize) -> Option<(usize, usize)> {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x == root_y {
+            return None;
+        }
+        let size_x = -self.parent[root_x];
+        let size_y = -self.parent[root_y];
+        self.size -= 1;
+        if size_x >= size_y {
+            self.parent[root_x] -= size_y;
+            self.parent[root_y] = root_x as isize;
+            Some((root_x, root_y))
+        } else {
+            self.parent[root_y] -= size_x;
+            self.parent[root_x] = root_y as isize;
+            Some((root_y, root_x))
+        }
+    }
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+    fn is_root(&mut self, x: usize) -> bool {
+        self.find(x) == x
+    }
+    fn get_union_size(&mut self, x: usize) -> usize {
+        let root = self.find(x);
+        -self.parent[root] as usize
+    }
+    fn get_size(&self) -> usize {
+        self.size
+    }
+}
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            S: Chars
+        }
+
+        let mut set = HashSet::new();
+        let mut stack = vec![];
+        let mut good_num = 0_usize;
+        let mut good_list = vec![HashSet::new(); 300_000];
+
+        for (i, s) in S.iter().enumerate() {
+            if *s == '(' {
+                stack.push(i);
+                good_num = i;
+            } else if *s == ')' {
+                let idx = stack.pop().unwrap();
+                let goods = good_list[idx].clone();
+                for g in goods {
+                    set.remove(g);
+                }
+                if !stack.is_empty() {
+                    good_num = stack.pop().unwrap();
+                    stack.push(good_num);
+                }
+            } else {
+                good_list[good_num].insert(s);
+                if set.contains(s) {
+                    println!("No");
+                    return;
+                } else {
+                    set.insert(s);
+                }
+            }
+        }
+        println!("Yes");
+    }
+}
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
