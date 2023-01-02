0b50f2b36067dc021cbdc28a5a7ca8603a1bd5a6
Yoke <tayok@synspective.com>
2023-01-02 17:19:49 +0900

    abc241c

--- abc241/src/bin/c.rs
+++ abc241/src/bin/c.rs
@@ -1,3 +1,150 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
+    }
+}
+#[derive(Debug, Clone)]
+struct UnionFind {
+    parent: Vec<isize>,
+    size: usize,
+}
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: vec![-1; n + 1],
+            size: n,
+        }
+    }
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] < 0 {
+            return x;
+        }
+        let root = self.find(self.parent[x] as usize);
+        self.parent[x] = root as isize;
+        root
+    }
+    fn unite(&mut self, x: usize, y: usize) -> Option<(usize, usize)> {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x == root_y {
+            return None;
+        }
+        let size_x = -self.parent[root_x];
+        let size_y = -self.parent[root_y];
+        self.size -= 1;
+        if size_x >= size_y {
+            self.parent[root_x] -= size_y;
+            self.parent[root_y] = root_x as isize;
+            Some((root_x, root_y))
+        } else {
+            self.parent[root_y] -= size_x;
+            self.parent[root_x] = root_y as isize;
+            Some((root_y, root_x))
+        }
+    }
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+    fn is_root(&mut self, x: usize) -> bool {
+        self.find(x) == x
+    }
+    fn get_union_size(&mut self, x: usize) -> usize {
+        let root = self.find(x);
+        -self.parent[root] as usize
+    }
+    fn get_size(&self) -> usize {
+        self.size
+    }
+}
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            N: usize,
+            S: [Chars; N]
+        }
+
+        let mut ok = false;
+        for i in 0..N {
+            for j in 0..N {
+                for (dr, dc) in [
+                    (1_isize, 0_isize),
+                    (0_isize, 1_isize),
+                    (1_isize, 1_isize),
+                    (1_isize, -1_isize),
+                ]
+                .iter()
+                {
+                    let mut cnt = 0_usize;
+                    if i as isize + dr * 5 >= N as isize
+                        || j as isize + dc * 5 >= N as isize
+                        || j as isize + dc * 5 < 0
+                    {
+                        continue;
+                    }
+                    for k in 0..6 {
+                        let row = (i as isize + dr * k) as usize;
+                        let col = (j as isize + dc * k) as usize;
+                        if S[row][col] == '#' {
+                            cnt += 1;
+                        }
+                    }
+                    if cnt >= 4 {
+                        ok = true;
+                    }
+                }
+            }
+        }
+        println!("{}", if ok { "Yes" } else { "No" });
+    }
+}
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+fn eratosthenes(n: usize) -> Vec<bool> {
+    let mut is_prime_list = vec![true; n + 1];
+    is_prime_list[0] = false;
+    is_prime_list[1] = false;
+    let mut i = 2;
+    while i * i <= n {
+        if is_prime_list[i] {
+            let mut j = i * i;
+            while j <= n {
+                is_prime_list[j] = false;
+                j += i;
+            }
+        }
+        i += 1
+    }
+    is_prime_list
 }
