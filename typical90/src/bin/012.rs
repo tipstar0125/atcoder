5e98b3b274683355960ca5d5b11d398712f457db
Yoke <tayok@synspective.com>
2023-01-21 15:10:39 +0900

    012

--- typical90/src/bin/012.rs
+++ typical90/src/bin/012.rs
@@ -1,3 +1,219 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use std::collections::BTreeMap;
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
+    }
+}
+#[derive(Debug, Clone)]
+struct UnionFind {
+    parent: Vec<isize>,
+    size: usize,
+}
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: vec![-1; n + 1],
+            size: n,
+        }
+    }
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] < 0 {
+            return x;
+        }
+        let root = self.find(self.parent[x] as usize);
+        self.parent[x] = root as isize;
+        root
+    }
+    fn unite(&mut self, x: usize, y: usize) -> Option<(usize, usize)> {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x == root_y {
+            return None;
+        }
+        let size_x = -self.parent[root_x];
+        let size_y = -self.parent[root_y];
+        self.size -= 1;
+        if size_x >= size_y {
+            self.parent[root_x] -= size_y;
+            self.parent[root_y] = root_x as isize;
+            Some((root_x, root_y))
+        } else {
+            self.parent[root_y] -= size_x;
+            self.parent[root_x] = root_y as isize;
+            Some((root_y, root_x))
+        }
+    }
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+    fn is_root(&mut self, x: usize) -> bool {
+        self.find(x) == x
+    }
+    fn get_union_size(&mut self, x: usize) -> usize {
+        let root = self.find(x);
+        -self.parent[root] as usize
+    }
+    fn get_size(&self) -> usize {
+        self.size
+    }
+}
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            H: usize,
+            W: usize,
+            Q: usize,
+        }
+
+        let mut matrix = vec![vec![false; W]; H];
+        let mut uf = UnionFind::new(H * W);
+
+        for _ in 0..Q {
+            input! {
+                t: usize
+            }
+
+            if t == 1 {
+                input! {
+                    r: Usize1,
+                    c: Usize1
+                }
+                matrix[r][c] = true;
+                let pos = r * W + c;
+                if r > 0 && matrix[r - 1][c] {
+                    let neighbor = (r - 1) * W + c;
+                    uf.unite(pos, neighbor);
+                }
+                if r < H - 1 && matrix[r + 1][c] {
+                    let neighbor = (r + 1) * W + c;
+                    uf.unite(pos, neighbor);
+                }
+                if c > 0 && matrix[r][c - 1] {
+                    let neighbor = r * W + c - 1;
+                    uf.unite(pos, neighbor);
+                }
+                if c < W - 1 && matrix[r][c + 1] {
+                    let neighbor = r * W + c + 1;
+                    uf.unite(pos, neighbor);
+                }
+            } else {
+                input! {
+                    ra: Usize1,
+                    ca: Usize1,
+                    rb: Usize1,
+                    cb: Usize1
+                }
+                let start = ra * W + ca;
+                let goal = rb * W + cb;
+                if matrix[ra][ca] && matrix[rb][cb] && uf.is_same(start, goal) {
+                    println!("Yes");
+                } else {
+                    println!("No");
+                }
+            }
+        }
+    }
+}
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+fn eratosthenes(n: usize) -> Vec<bool> {
+    let mut is_prime_list = vec![true; n + 1];
+    is_prime_list[0] = false;
+    is_prime_list[1] = false;
+    let mut i = 2;
+    while i * i <= n {
+        if is_prime_list[i] {
+            let mut j = i * i;
+            while j <= n {
+                is_prime_list[j] = false;
+                j += i;
+            }
+        }
+        i += 1
+    }
+    is_prime_list
+}
+
+fn mod_pow(a: usize, b: usize, m: usize) -> usize {
+    let mut p = a;
+    let mut ret = 1;
+    let mut n = b;
+    while n > 0 {
+        if n & 1 == 1 {
+            ret = ret * p % m;
+        }
+        p = p * p % m;
+        n >>= 1;
+    }
+    ret
+}
+
+fn mod_div(a: usize, b: usize, m: usize) -> usize {
+    (a * mod_pow(b, m - 2, m)) % m
+}
+
+fn prime_factorize(n: usize) -> BTreeMap<usize, usize> {
+    let mut nn = n;
+    let mut i = 2;
+    let mut pf: BTreeMap<usize, usize> = BTreeMap::new();
+    while i * i <= n {
+        while nn % i == 0 {
+            *pf.entry(i).or_default() += 1;
+            nn /= i;
+        }
+        i += 1;
+    }
+    if nn != 1 {
+        *pf.entry(nn).or_default() += 1;
+    }
+    pf
+}
+
+fn enum_dividers(n: usize) -> Vec<usize> {
+    let mut i = 1_usize;
+    let mut ret = vec![];
+    while i * i <= n {
+        if n % i == 0 {
+            ret.push(i);
+            if i != n / i {
+                ret.push(n / i);
+            }
+        }
+        i += 1;
+    }
+    ret.sort();
+    ret
 }
