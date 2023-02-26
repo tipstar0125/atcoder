a8fe232fd64365f5d7b5b5fb85e13f4199b7c585
Yoke <tayok@synspective.com>
2023-02-26 16:26:45 +0900

    abc278e

--- abc278/src/bin/e.rs
+++ abc278/src/bin/e.rs
@@ -1,3 +1,215 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use std::collections::BTreeMap;
+
+use itertools::Itertools;
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
+    }
+}
+#[derive(Debug, Clone)]
+struct UnionFind {
+    parent: Vec<isize>,
+    size: usize,
+}
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            parent: vec![-1; n + 1],
+            size: n,
+        }
+    }
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] < 0 {
+            return x;
+        }
+        let root = self.find(self.parent[x] as usize);
+        self.parent[x] = root as isize;
+        root
+    }
+    fn unite(&mut self, x: usize, y: usize) -> Option<(usize, usize)> {
+        let root_x = self.find(x);
+        let root_y = self.find(y);
+        if root_x == root_y {
+            return None;
+        }
+        let size_x = -self.parent[root_x];
+        let size_y = -self.parent[root_y];
+        self.size -= 1;
+        if size_x >= size_y {
+            self.parent[root_x] -= size_y;
+            self.parent[root_y] = root_x as isize;
+            Some((root_x, root_y))
+        } else {
+            self.parent[root_y] -= size_x;
+            self.parent[root_x] = root_y as isize;
+            Some((root_y, root_x))
+        }
+    }
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+    fn is_root(&mut self, x: usize) -> bool {
+        self.find(x) == x
+    }
+    fn get_union_size(&mut self, x: usize) -> usize {
+        let root = self.find(x);
+        -self.parent[root] as usize
+    }
+    fn get_size(&self) -> usize {
+        self.size
+    }
+}
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            H: usize,
+            W: usize,
+            _N: usize,
+            h: usize,
+            w: usize,
+            A: [[usize; W]; H]
+        }
+
+        let mut mp: BTreeMap<usize, usize> = BTreeMap::new();
+        for i in 0..H {
+            for j in 0..W {
+                *mp.entry(A[i][j]).or_default() += 1;
+            }
+        }
+        let cnt = mp.len();
+
+        for i in 0..H - h + 1 {
+            let mut mp2 = mp.clone();
+            let mut cnt2 = cnt;
+            for ii in i..i + h {
+                for jj in 0..w {
+                    let a = A[ii][jj];
+                    *mp2.entry(a).or_default() -= 1;
+                    if mp2[&a] == 0 {
+                        cnt2 -= 1;
+                    }
+                }
+            }
+            let mut ans = vec![];
+            ans.push(cnt2);
+            for j in 0..W - w {
+                for ii in i..i + h {
+                    let a_added = A[ii][j];
+                    let a_removed = A[ii][j + w];
+                    *mp2.entry(a_added).or_default() += 1;
+                    if mp2[&a_added] == 1 {
+                        cnt2 += 1;
+                    }
+                    *mp2.entry(a_removed).or_default() -= 1;
+                    if mp2[&a_removed] == 0 {
+                        cnt2 -= 1;
+                    }
+                }
+                ans.push(cnt2);
+            }
+            println!("{}", ans.iter().join(" "));
+        }
+    }
+}
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+fn eratosthenes(n: usize) -> Vec<bool> {
+    let mut is_prime_list = vec![true; n + 1];
+    is_prime_list[0] = false;
+    is_prime_list[1] = false;
+    let mut i = 2;
+    while i * i <= n {
+        if is_prime_list[i] {
+            let mut j = i * i;
+            while j <= n {
+                is_prime_list[j] = false;
+                j += i;
+            }
+        }
+        i += 1
+    }
+    is_prime_list
+}
+
+fn mod_pow(a: usize, b: usize, m: usize) -> usize {
+    let mut p = a;
+    let mut ret = 1;
+    let mut n = b;
+    while n > 0 {
+        if n & 1 == 1 {
+            ret = ret * p % m;
+        }
+        p = p * p % m;
+        n >>= 1;
+    }
+    ret
+}
+
+fn mod_div(a: usize, b: usize, m: usize) -> usize {
+    (a * mod_pow(b, m - 2, m)) % m
+}
+
+fn prime_factorize(n: usize) -> BTreeMap<usize, usize> {
+    let mut nn = n;
+    let mut i = 2;
+    let mut pf: BTreeMap<usize, usize> = BTreeMap::new();
+    while i * i <= n {
+        while nn % i == 0 {
+            *pf.entry(i).or_default() += 1;
+            nn /= i;
+        }
+        i += 1;
+    }
+    if nn != 1 {
+        *pf.entry(nn).or_default() += 1;
+    }
+    pf
+}
+
+fn enum_dividers(n: usize) -> Vec<usize> {
+    let mut i = 1_usize;
+    let mut ret = vec![];
+    while i * i <= n {
+        if n % i == 0 {
+            ret.push(i);
+            if i != n / i {
+                ret.push(n / i);
+            }
+        }
+        i += 1;
+    }
+    ret.sort();
+    ret
 }
