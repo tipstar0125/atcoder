50b7e2bc8fb72c599cf171b6e21635c4666b530f
Yoke <06106186@gmail.com>
2023-05-23 19:36:58 +0900

    abc233e

--- abc233/src/bin/e.rs
+++ abc233/src/bin/e.rs
@@ -1,3 +1,66 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use proconio::marker::Bytes;
+use std::cmp::Reverse;
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+use superslice::Ext;
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            X: Bytes
+        }
+
+        let L = X.len();
+
+        let mut s = 0;
+        for i in 0..L {
+            let num = (X[i] - b'0') as usize;
+            s += num;
+        }
+
+        let mut ans = vec![0; L + 11];
+        for i in 0..L + 10 {
+            let mut ss = ans[i];
+            ss += s;
+            ans[i] = ss % 10;
+            ans[i + 1] = ss / 10;
+            if i < L {
+                s -= (X[L - i - 1] - b'0') as usize;
+            }
+        }
+        for i in (0..L + 11).rev() {
+            if ans[i] == 0 {
+                ans.pop();
+            } else {
+                break;
+            }
+        }
+        ans.reverse();
+        println!("{}", ans.iter().join(""));
+    }
+}
+
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
 }
