3a86f2947d6fb7509025cf1bd3d222c854b43d28
Yoke <06106186@gmail.com>
2023-05-31 23:51:26 +0900

    a50 modified

--- tessoku-book/src/bin/a50.rs
+++ tessoku-book/src/bin/a50.rs
@@ -158,6 +158,32 @@ impl State {
         }
         score
     }
+    fn evaluate_score(&self, x: usize, y: usize, h: usize) -> isize {
+        let mut score = self.score;
+        let penalty = 100;
+        for i in 0..N {
+            for j in 0..N {
+                let manhattan_dist =
+                    (i as isize - x as isize).abs() + (j as isize - y as isize).abs();
+                if manhattan_dist >= h as isize {
+                    continue;
+                }
+                let add_h = h as isize - manhattan_dist;
+                let a = A[j][i];
+                let mountain = self.B[j][i];
+                let mut before_diff = (a - mountain).abs();
+                if mountain >= a {
+                    before_diff += penalty;
+                }
+                let mut now_diff = (a - (mountain + add_h)).abs();
+                if mountain + add_h >= a {
+                    now_diff += penalty;
+                }
+                score += before_diff - now_diff;
+            }
+        }
+        score
+    }
     fn change(&mut self, query0: (usize, usize, usize), query1: (usize, usize, usize)) {
         let (x0, y0, h0) = query0;
         let (x1, y1, h1) = query1;
@@ -198,6 +224,24 @@ fn init(state: &mut State, query: &mut [(usize, usize, usize)]) {
     state.score = state.get_score();
 }
 
+fn init_beam(state: &mut State, beam_width: usize, query: &mut [(usize, usize, usize)]) {
+    for i in 0..MAX_Q {
+        let mut beam = BinaryHeap::new();
+        for _ in 0..beam_width {
+            let x = rnd::gen_range(0, N);
+            let y = rnd::gen_range(0, N);
+            let h = rnd::gen_range(1, N + 1);
+            let score = state.evaluate_score(x, y, h);
+            beam.push((score, x, y, h));
+        }
+        let (_, x, y, h) = beam.pop().unwrap();
+        assert!(state.is_legal_action(x, y, h));
+        state.change(query[i], (x, y, h));
+        query[i] = (x, y, h);
+    }
+    state.score = state.get_score();
+}
+
 #[derive(Default)]
 struct Solver {}
 impl Solver {
@@ -220,7 +264,9 @@ impl Solver {
         let mut state = State::new();
         let mut query = vec![(0, 0, 0); MAX_Q];
 
-        init(&mut state, &mut query);
+        init_beam(&mut state, 5, &mut query);
+        eprintln!("Initialize finish time: {}sec", time_keeper.get_time());
+        eprintln!("Initialize finish score: {}", state.score);
         let mut cnt = 0;
 
         let start_temp = 180.0;
@@ -232,15 +278,15 @@ impl Solver {
             let idx = rnd::gen_range(0, MAX_Q);
             let current_score = state.score;
             let (x0, y0, h0) = query[idx];
-            let dx = rnd::gen_range_neg_wrapping(5);
-            let dy = rnd::gen_range_neg_wrapping(5);
-            if time_keeper.get_time() > 0.5 {
+            let dx = rnd::gen_range_neg_wrapping(3);
+            let dy = rnd::gen_range_neg_wrapping(3);
+            if time_keeper.get_time() > 1.0 {
                 h_limit = 8;
             }
             if current_score >= 199_900_000 {
                 h_limit = 2;
             } else if current_score >= 199_500_000 {
-                h_limit = 8;
+                h_limit = 5;
             }
             let dh = rnd::gen_range_neg_wrapping(h_limit);
             let x = x0.wrapping_add(dx);
