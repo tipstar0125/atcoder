f9d51df73179e834b5395c827bbc71449ca5b072
Yoke <06106186@gmail.com>
2023-06-10 15:39:35 +0900

    abc307 mobius

--- abc304/src/bin/f_.rs
+++ abc304/src/bin/f_.rs
@@ -28,110 +28,55 @@ impl Solver {
             S: Chars
         }
 
-        let E = Eratosthenes::new(N);
-        let mut ans = Mod::zero();
+        let e = enum_dividers(N);
+        let L = e.len() - 1;
+        let mut dp = vec![Mod::zero(); L];
 
-        for &t in &E.dividers(N) {
-            if t == N {
-                break;
-            }
+        for i in 0..L {
+            let n = e[i];
             let mut pos = 0_usize;
-            let mut v = vec![true; t];
+            let mut v = vec![true; n];
             while pos < N {
-                let s = &S[pos..pos + t];
+                let s = &S[pos..pos + n];
                 for (i, si) in s.iter().enumerate() {
                     if *si == '.' {
                         v[i] = false;
                     }
                 }
-                pos += t;
+                pos += n;
             }
-            let exp = v.iter().filter(|&&x| x).count();
-            let cnt = Mod::new(2).pow(exp);
-            if E.mobius[N / t] == 1 {
-                ans -= cnt;
-            } else if E.mobius[N / t] == -1 {
-                ans += cnt;
+            let c = v.iter().filter(|&&x| x).count();
+            dp[i] = Mod::new(2).pow(c);
+            for j in 0..i {
+                let nb = e[j];
+                if n % nb == 0 {
+                    let z = dp[j];
+                    dp[i] -= z;
+                }
             }
         }
-        println!("{}", ans.value());
+        let mut ans = Mod::zero();
+        for i in 0..L {
+            ans += dp[i];
+        }
+        println!("{}", ans.value);
     }
 }
 
-#[derive(Debug, Clone, Default)]
-struct Eratosthenes {
-    is_prime: Vec<bool>,
-    min_factor: Vec<isize>,
-    mobius: Vec<isize>,
-}
-
-impl Eratosthenes {
-    fn new(n: usize) -> Self {
-        let mut is_prime = vec![true; n + 1];
-        let mut min_factor = vec![-1; n + 1];
-        let mut mobius = vec![1; n + 1];
-        is_prime[0] = false;
-        is_prime[1] = false;
-        min_factor[1] = 1;
-
-        for p in 2..=n {
-            if !is_prime[p] {
-                continue;
-            }
-            min_factor[p] = p as isize;
-            mobius[p] = -1;
-
-            let mut q = p * 2;
-            while q <= n {
-                is_prime[q] = false;
-                if min_factor[q] == -1 {
-                    min_factor[q] = p as isize;
-                }
-                if (q / p) % p == 0 {
-                    mobius[q] = 0;
-                } else {
-                    mobius[q] = -mobius[q];
-                }
-                q += p;
+fn enum_dividers(n: usize) -> Vec<usize> {
+    let mut i = 1_usize;
+    let mut ret = vec![];
+    while i * i <= n {
+        if n % i == 0 {
+            ret.push(i);
+            if i != n / i {
+                ret.push(n / i);
             }
         }
-        Eratosthenes {
-            is_prime,
-            min_factor,
-            mobius,
-        }
+        i += 1;
     }
-    fn factorize(&self, N: usize) -> Vec<(usize, usize)> {
-        let mut ret = vec![];
-        let mut n = N;
-        while n > 1 {
-            let p = self.min_factor[n];
-            let mut exp = 0;
-
-            while self.min_factor[n] == p {
-                n /= p as usize;
-                exp += 1;
-            }
-            ret.push((p as usize, exp));
-        }
-        ret
-    }
-    fn dividers(&self, N: usize) -> Vec<usize> {
-        let mut ret = vec![1];
-        let pf = self.factorize(N);
-        for &(p, exp) in &pf {
-            let m = ret.len();
-            for i in 0..m {
-                let mut v = 1;
-                for _ in 0..exp {
-                    v *= p;
-                    ret.push(ret[i] * v);
-                }
-            }
-        }
-        ret.sort();
-        ret
-    }
+    ret.sort();
+    ret
 }
 
 type Mod = ModInt;
