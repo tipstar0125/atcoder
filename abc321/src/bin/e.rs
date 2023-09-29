070fcc1603904dcb73539ddb3935cedbd1460f4c
Yoke <06106186@gmail.com>
2023-09-29 20:02:35 +0900

    abc321e

--- abc321/src/bin/e.rs
+++ abc321/src/bin/e.rs
@@ -7,7 +7,6 @@
 #![allow(clippy::neg_multiply)]
 #![allow(dead_code)]
 use itertools::Itertools;
-use superslice::Ext;
 use std::cmp::Reverse;
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
 
@@ -21,7 +20,70 @@ struct Solver {}
 impl Solver {
     #[fastout]
     fn solve(&mut self) {
-        input! {}
+        input! {
+            T: usize
+        }
+
+        for _ in 0..T {
+            input! {
+                N: usize,
+                mut X: usize,
+                mut K: usize,
+            }
+
+            if K == 0 {
+                println!("1");
+                continue;
+            }
+
+            let mut ans = count(X, K, N);
+
+            loop {
+                if X == 1 || K == 1 {
+                    break;
+                }
+                ans += count(X ^ 1, K - 2, N);
+                X >>= 1;
+                K -= 1;
+            }
+            if X > 1 {
+                ans += 1;
+            }
+
+            println!("{}", ans);
+        }
+    }
+}
+
+fn count(now: usize, d: usize, N: usize) -> usize {
+    let mut l = now;
+    let mut r = now;
+    let m = min!(d, now.leading_zeros() as usize);
+    l <<= m;
+    r <<= m;
+    r += (1_usize << m) - 1;
+    if l > N {
+        return 0;
+    }
+    if r <= N {
+        return r - l + 1;
+    } else {
+        return N - l + 1;
+    }
+}
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
     }
 }
 
