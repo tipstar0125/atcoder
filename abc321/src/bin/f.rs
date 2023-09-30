3b773107f8b111a225707fdf58c20cb3fe2b3d7f
Yoke <06106186@gmail.com>
2023-09-30 09:33:58 +0900

    abc321f

--- abc321/src/bin/f.rs
+++ abc321/src/bin/f.rs
@@ -7,7 +7,6 @@
 #![allow(clippy::neg_multiply)]
 #![allow(dead_code)]
 use itertools::Itertools;
-use superslice::Ext;
 use std::cmp::Reverse;
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
 
@@ -16,12 +15,144 @@ use proconio::{
     marker::{Chars, Usize1},
 };
 
+const MOD: usize = 998244353;
+
 #[derive(Default)]
 struct Solver {}
 impl Solver {
     #[fastout]
     fn solve(&mut self) {
-        input! {}
+        input! {
+            Q: usize,
+            K: usize,
+        }
+
+        let mut dp = vec![vec![Mod::zero(); K + 1]; Q + 1];
+        dp[0][0] = Mod::one();
+
+        for q in 1..=Q {
+            input! {
+                t: char,
+                x: usize
+            }
+            if t == '+' {
+                for k in 0..=K {
+                    let z = dp[q - 1][k];
+                    dp[q][k] += z;
+                    if k + x <= K {
+                        dp[q][k + x] += z;
+                    }
+                }
+            } else {
+                for k in 0..=K {
+                    let z = dp[q - 1][k];
+                    dp[q][k] += z;
+                }
+                for k in x..=K {
+                    let z = dp[q][k - x];
+                    dp[q][k] -= z;
+                }
+            }
+            println!("{}", dp[q][K].value());
+        }
+    }
+}
+
+type Mod = ModInt;
+#[derive(Debug, Clone, Copy, Default)]
+struct ModInt {
+    value: usize,
+}
+
+impl ModInt {
+    fn new(n: usize) -> Self {
+        ModInt { value: n % MOD }
+    }
+    fn zero() -> Self {
+        ModInt { value: 0 }
+    }
+    fn one() -> Self {
+        ModInt { value: 1 }
+    }
+    fn value(&self) -> usize {
+        self.value
+    }
+    fn pow(&self, n: usize) -> Self {
+        let mut p = *self;
+        let mut ret = ModInt::one();
+        let mut nn = n;
+        while nn > 0 {
+            if nn & 1 == 1 {
+                ret *= p;
+            }
+            p *= p;
+            nn >>= 1;
+        }
+        ret
+    }
+    fn inv(&self) -> Self {
+        fn ext_gcd(a: usize, b: usize) -> (isize, isize, usize) {
+            if a == 0 {
+                return (0, 1, b);
+            }
+            let (x, y, g) = ext_gcd(b % a, a);
+            (y - b as isize / a as isize * x, x, g)
+        }
+
+        ModInt::new((ext_gcd(self.value, MOD).0 + MOD as isize) as usize)
+    }
+}
+
+impl std::ops::Add for ModInt {
+    type Output = ModInt;
+    fn add(self, other: Self) -> Self {
+        ModInt::new(self.value + other.value)
+    }
+}
+
+impl std::ops::Sub for ModInt {
+    type Output = ModInt;
+    fn sub(self, other: Self) -> Self {
+        ModInt::new(MOD + self.value - other.value)
+    }
+}
+
+impl std::ops::Mul for ModInt {
+    type Output = ModInt;
+    fn mul(self, other: Self) -> Self {
+        ModInt::new(self.value * other.value)
+    }
+}
+
+#[allow(clippy::suspicious_arithmetic_impl)]
+impl std::ops::Div for ModInt {
+    type Output = ModInt;
+    fn div(self, other: Self) -> Self {
+        self * other.inv()
+    }
+}
+
+impl std::ops::AddAssign for ModInt {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other;
+    }
+}
+
+impl std::ops::SubAssign for ModInt {
+    fn sub_assign(&mut self, other: Self) {
+        *self = *self - other;
+    }
+}
+
+impl std::ops::MulAssign for ModInt {
+    fn mul_assign(&mut self, other: Self) {
+        *self = *self * other;
+    }
+}
+
+impl std::ops::DivAssign for ModInt {
+    fn div_assign(&mut self, other: Self) {
+        *self = *self / other;
     }
 }
 
