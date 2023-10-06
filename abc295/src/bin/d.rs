82a11493f13ee231df65fe62b6b5c92445cfff30
Yoke <06106186@gmail.com>
2023-10-06 13:20:32 +0900

    abc295d

--- abc295/src/bin/d.rs
+++ abc295/src/bin/d.rs
@@ -1,3 +1,53 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use std::cmp::Reverse;
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+use superslice::Ext;
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            S: Chars
+        }
+
+        let mut cnt = vec![0_usize; 1 << 10];
+        let mut x = 0;
+        cnt[x] += 1;
+        for c in S {
+            let n = c as u8 - b'0';
+            x ^= 1 << n;
+            cnt[x] += 1;
+        }
+        let mut ans = 0;
+        for i in 0..1 << 10 {
+            if cnt[i] >= 2 {
+                ans += cnt[i] * (cnt[i] - 1) / 2;
+            }
+        }
+        println!("{}", ans);
+    }
+}
+
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
 }
