cfe0dca25e08f01a236310d8eb5ec2ab6c129ac1
Yoke <06106186@gmail.com>
2023-10-20 15:47:44 +0900

    abc270d

--- abc270/src/bin/d.rs
+++ abc270/src/bin/d.rs
@@ -1,1 +1,64 @@
-fn main() {}
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use std::cmp::Reverse;
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+use superslice::Ext;
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            N: usize,
+            K: usize,
+            A: [usize; K]
+        }
+
+        let mut dp = vec![0; N + 1];
+        for i in 1..=N {
+            for &a in &A {
+                if i >= a {
+                    dp[i] = max!(dp[i], a + i - a - dp[i - a]);
+                }
+            }
+        }
+        println!("{}", dp[N]);
+    }
+}
+
+#[macro_export]
+macro_rules! max {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::max($x, max!($( $y ),+))
+    }
+}
+#[macro_export]
+macro_rules! min {
+    ($x: expr) => ($x);
+    ($x: expr, $( $y: expr ),+) => {
+        std::cmp::min($x, min!($( $y ),+))
+    }
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
+}
