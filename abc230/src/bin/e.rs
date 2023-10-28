356551645cbfb31e1db29d1b2a8bb591a6b8ce8a
Yoke <06106186@gmail.com>
2023-10-28 17:57:59 +0900

    abc230e

--- abc230/src/bin/e.rs
+++ abc230/src/bin/e.rs
@@ -1,3 +1,57 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use std::cmp::Reverse;
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+use superslice::Ext;
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            N: usize
+        }
+
+        let mut ans = 0;
+        let mut l = 1;
+        let mut r = 1;
+        while r <= N {
+            let mut ng = l;
+            let mut ok = N + 1;
+            while ok - ng > 1 {
+                let m = (ok + ng) / 2;
+                if N / m < N / l {
+                    ok = m;
+                } else {
+                    ng = m;
+                }
+            }
+            r = ok;
+            ans += (r - l) * (N / l);
+            l = r;
+        }
+        println!("{}", ans);
+    }
+}
+
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
 }
