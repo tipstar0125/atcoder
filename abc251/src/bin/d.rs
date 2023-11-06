d589dd1f03812f7a3841066bf98e14acf3aa6832
Yoke <06106186@gmail.com>
2023-11-06 12:50:17 +0900

    abc251d

--- abc251/src/bin/d.rs
+++ abc251/src/bin/d.rs
@@ -1,3 +1,46 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+#![allow(clippy::needless_range_loop)]
+#![allow(clippy::comparison_chain)]
+#![allow(clippy::nonminimal_bool)]
+#![allow(clippy::neg_multiply)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use std::cmp::Reverse;
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
+use superslice::Ext;
+
+use proconio::{
+    fastout, input,
+    marker::{Chars, Usize1},
+};
+
+#[derive(Default)]
+struct Solver {}
+impl Solver {
+    #[fastout]
+    fn solve(&mut self) {
+        input! {
+            _: usize
+        }
+
+        let mut ans = vec![];
+        for i in 1..=99 {
+            ans.push(i);
+            ans.push(i * 100);
+            ans.push(i * 10000);
+        }
+        println!("{}", ans.len());
+        println!("{}", ans.iter().join(" "));
+    }
+}
+
 fn main() {
-    todo!();
+    std::thread::Builder::new()
+        .stack_size(128 * 1024 * 1024)
+        .spawn(|| Solver::default().solve())
+        .unwrap()
+        .join()
+        .unwrap();
 }
